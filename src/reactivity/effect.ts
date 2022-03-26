f956e8d71f0ca38608a3aef7edd11b9914042eda
kongcodes <2724355732@qq.com>
2022-03-26 23:43:33 +0800

    实现effect stop & onStop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,59 +1,96 @@
+import { extend } from "../shared";
 
-class reactiveEffect{
-  private _fn: any;
+class reactiveEffect {
+	private _fn: any;
+	public scheduler: Function | undefined;
+	// stop
+	deps = [];
+	active = true;
+	onStop?: () => void;
 
-  constructor(fn, public scheduler?){
-    this._fn = fn;
-  }
+	constructor(fn, scheduler?: Function) {
+		this._fn = fn;
+		this.scheduler = scheduler;
+	}
 
-  run(){
-    activeEffect = this;
-    // runner方法需要得到fn的返回值
-    return this._fn();
-  }
+	run() {
+		activeEffect = this;
+		// runner方法需要得到fn的返回值
+		return this._fn();
+	}
+
+	stop() {
+		if (this.active) {
+			cleanupEffect(this);
+			// stop时 调用onStop方法
+			if (this.onStop) {
+				this.onStop();
+			}
+			this.active = false;
+		}
+	}
+}
+
+function cleanupEffect(effect) {
+	effect.deps.forEach((dep: any) => {
+		dep.delete(effect);
+	});
 }
 
 const targetMap = new Map();
 export function track(target, key) {
-  // target -> key -> dep
-  // targetMap -> { target: depsMap }
-  // depsMap -> {key: dep}
-  // dep -> activeEffect
+	// target -> key -> dep
+	// targetMap -> { target: depsMap }
+	// depsMap -> {key: dep}
+	// dep -> activeEffect
 
-  let depsMap = targetMap.get(target);
-  if(!depsMap){
-    depsMap = new Map();
-    targetMap.set(target, depsMap);
-  }
+	let depsMap = targetMap.get(target);
+	if (!depsMap) {
+		depsMap = new Map();
+		targetMap.set(target, depsMap);
+	}
 
-  let dep = depsMap.get(key);
-  if(!dep){
-    dep = new Set();
-    depsMap.set(key, dep);
-  }
+	let dep = depsMap.get(key);
+	if (!dep) {
+		dep = new Set();
+		depsMap.set(key, dep);
+	}
 
-  dep.add(activeEffect);
+	dep.add(activeEffect);
+	// track的时候收集dep，stop会用到
+	if (!activeEffect) return; //解决只用reactive时，deps undefined的情况
+	activeEffect.deps.push(dep);
 }
 
-export function trigger(target, key){
-  const depsMap = targetMap.get(target);
-  const dep = depsMap.get(key);
-  for (const effect of dep) {
-    if(effect.scheduler){
-      effect.scheduler()
-    }else{
-      effect.run();
-    }
-  }
+export function trigger(target, key) {
+	const depsMap = targetMap.get(target);
+	const dep = depsMap.get(key);
+	for (const effect of dep) {
+		if (effect.scheduler) {
+			effect.scheduler();
+		} else {
+			effect.run();
+		}
+	}
 }
 
 let activeEffect;
-export function effect(fn, options:any = {}){
-  const _effect = new reactiveEffect(fn, options.scheduler);
+export function effect(fn, options: any = {}) {
+	const _effect = new reactiveEffect(fn, options.scheduler);
 
-  _effect.run();
+	_effect.run();
 
-  // 返回的 runner函数
-  // run方法用到了this,使用bind处理指针问题
-  return _effect.run.bind(_effect);
+	// _effect.onStop = options.onStop; // 抽离-优化
+	extend(_effect, options);
+
+	// 返回的 runner函数
+	// run方法用到了this,使用bind处理指针问题
+	const runner: any = _effect.run.bind(_effect);
+	// stop 要用到 _effect上面的方法
+	runner.effect = _effect;
+	return runner;
+}
+
+export function stop(runner) {
+	runner.effect.stop();
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,52 +1,50 @@
-import { effect } from "../effect";
+import { effect, stop } from "../effect";
 import { reactive } from "../reactive";
 
 describe("effect", () => {
 	it("happy path", () => {
-
 		const obj = reactive({ num: 10 });
 		let addnum;
 		effect(() => {
 			addnum = obj.num + 1;
 		});
-    expect(addnum).toBe(11);
+		expect(addnum).toBe(11);
 
-    // update
-    obj.num++;
-    expect(addnum).toBe(12);
-
+		// update
+		obj.num++;
+		expect(addnum).toBe(12);
 	});
 
-	it('runner', () => {
+	it("runner", () => {
 		// effect(fn) -> runner -> const r =  runner() -> fn() & r
 		let foo = 10;
-		const runner = effect(()=>{
+		const runner = effect(() => {
 			foo++;
-			return 'foo'
-		})
+			return "foo";
+		});
 		expect(foo).toBe(11);
 		const r = runner();
 		expect(foo).toBe(12);
-		expect(r).toBe('foo');
+		expect(r).toBe("foo");
 	});
 
-	it('scheduler', () => {
+	it("scheduler", () => {
 		// 1.给effect(fn)传入第二个参数 options -> { scheduler: fn2}，也就是名为 scheduler 的一个函数
 		// 2.effect 首次还会执行fn，不会执行fn2
 		// 3.但如果scheduler存在，则响应式set updata 的时候不再执行fn而是执行fn2
 		// 4.执行runner的时候会再次执行fn
 		let dummy = 1;
-		let run:any;
-		const obj = reactive({num: 1});
-		const scheduler = jest.fn(()=>{
+		let run: any;
+		const obj = reactive({ num: 1 });
+		const scheduler = jest.fn(() => {
 			run = runner;
-		})
+		});
 		const runner = effect(
-			()=>{
+			() => {
 				dummy = obj.num;
 			},
-			{scheduler}
-		)
+			{ scheduler }
+		);
 		// first -> be called fn,  not call scheduler
 		expect(scheduler).not.toHaveBeenCalled();
 		expect(dummy).toBe(1);
@@ -58,5 +56,39 @@ describe("effect", () => {
 		// manually run -> fn()
 		run();
 		expect(dummy).toBe(2);
+	});
+
+	it("stop", () => {
+		let dummy;
+		const obj = reactive({ num: 1 });
+		const runner = effect(() => {
+			dummy = obj.num;
+		});
+		expect(dummy).toBe(1);
+		obj.num = 2;
+		expect(dummy).toBe(2);
+		stop(runner);
+		obj.num = 3;
+		expect(dummy).toBe(2);
+
+		// stopped effect should still be manually called callable
+		runner();
+		expect(dummy).toBe(3);
+	});
+
+	it("onStop", () => {
+		// 执行 stop 方法后，会调用onStop函数，和 scheduler 相似
+		const obj = reactive({ foo: 1 });
+		const onStop = jest.fn();
+		let dummy;
+		const runner = effect(
+			() => {
+				dummy = obj.foo;
+			},
+			{ onStop }
+		);
+
+		stop(runner);
+		expect(onStop).toBeCalledTimes(1);
 	});
 });
