a9840f4265746e3817af1d60598ae072358e7a28
kongcodes <78190504+kongcodes@users.noreply.github.com>
2022-04-26 16:45:04 +0800

    双端对比 diff 算法(2) 中间对比 删除逻辑及key的应用

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -77,12 +77,58 @@ import { h, ref } from '../../lib/k-mini-vue3.esm.js';
  *  A (B, C)
  *    (B, C)
  */
+// const prevChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C' }, 'C'),
+// ];
+// const nextChildren = [h('p', { key: 'B' }, 'B'), h('p', { key: 'C' }, 'C')];
+
+/**
+ * 5. 中间对比
+ *   5.1 删除老的
+ *   D 节点在新的里没有 要删除
+ *   C 节点 props 发生变化
+ */
+// const prevChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C', id: 'c-prev' }, 'C'),
+//   h('p', { key: 'D' }, 'D'),
+//   h('p', { key: 'F' }, 'F'),
+//   h('p', { key: 'G' }, 'G'),
+// ];
+// const nextChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'E' }, 'E'),
+//   h('p', { key: 'C', id: 'c-next' }, 'C'),
+//   h('p', { key: 'F' }, 'F'),
+//   h('p', { key: 'G' }, 'G'),
+// ];
+
+// 优化删除逻辑：
 const prevChildren = [
   h('p', { key: 'A' }, 'A'),
   h('p', { key: 'B' }, 'B'),
-  h('p', { key: 'C' }, 'C'),
+  h('p', { key: 'C', id: 'c-prev' }, 'C'),
+  h('p', { key: 'E' }, 'E'),
+  h('p', { key: 'D' }, 'D'),
+  h('p', { key: 'F' }, 'F'),
+  h('p', { key: 'G' }, 'G'),
 ];
-const nextChildren = [h('p', { key: 'B' }, 'B'), h('p', { key: 'C' }, 'C')];
+const nextChildren = [
+  h('p', { key: 'A' }, 'A'),
+  h('p', { key: 'B' }, 'B'),
+  h('p', { key: 'E' }, 'E'),
+  h('p', { key: 'C', id: 'c-next' }, 'C'),
+  h('p', { key: 'F' }, 'F'),
+  h('p', { key: 'G' }, 'G'),
+];
+
+/**
+ * 控制台输入 isChange.value = true;
+ */
 
 export default {
   name: 'ArrayToArray',
