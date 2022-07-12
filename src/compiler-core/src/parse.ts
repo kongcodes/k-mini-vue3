420c455cba267b5529af1d3f679f7ee115de10b8
kongcodes <78190504+kongcodes@users.noreply.github.com>
2022-07-12 22:38:00 +0800

    实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -3,32 +3,48 @@ import { NodeTypes, TagTypes } from "./ast";
 export function baseParse(content: string) {
 
   const context = createParserContext(content);
-  return createRoot(parseChildren(context));
+  return createRoot(parseChildren(context, "")); // 初始 parentTag 为空
 }
 
-function parseChildren(context) {
+function parseChildren(context, parentTag) {
   const nodes: any = [];
-  let node;
-  const s = context.source;
-  if (s.startsWith("{{")) {
-    node = parseInterpolation(context);
-  } else if (s[0] === "<") {
-    if (/[a-z]/i.test(s[1])) {
-      node = parseElement(context);
+
+  // 重复执行，没有结束的时候就一直重复执行
+  while(!isEnd(context, parentTag)){
+    let node;
+    const s = context.source;
+    if (s.startsWith("{{")) {
+      node = parseInterpolation(context);
+    } else if (s[0] === "<") {
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context);
+      }
     }
+  
+    // 解析 Text
+    if(!node){
+      node = parseText(context);
+    }
+  
+    nodes.push(node);
   }
 
-  // 解析 Text
-  if(!node){
-    node = parseText(context);
-  }
-
-  nodes.push(node);
   return nodes;
 }
 
 function parseText(context: any){
-  const content = parseTextData(context, context.source.length);
+
+  let endIndex = context.source.length;
+  let endToken = "{{";
+
+  const index = context.source.indexOf(endToken);
+  if (index !== -1) {
+    endIndex = index;
+  }
+
+  // 获取 content
+  const content = parseTextData(context, endIndex);
+  console.log("content -----------", content);
 
   return {
     type: NodeTypes.TEXT,
@@ -37,13 +53,17 @@ function parseText(context: any){
 }
 
 function parseElement(context: any) {
-  const element = parseTag(context, TagTypes.START);
+  // 1. 解析两个 tag
+  const element: any = parseTag(context, TagTypes.START);
+  // 2. 解析 tag 之间的 children
+  element.children = parseChildren(context, element.tag);
   parseTag(context, TagTypes.END);
   // console.log("-------", context.source);
   return element;
 }
 
 function parseTag(context: any, type: TagTypes) {
+  console.log(context);
   // 1.解析 tag
   const match: any = /^<\/?([a-z]*)/i.exec(context.source); //<d 或者 </d 开头，因为要两次处理，第一次是开始标签第二次是结束标签
   const tag = match[1]; // -> div
@@ -109,4 +129,14 @@ function parseTextData(context: any, length) {
 }
 function advanceBy(context: any, length: number) {
   context.source = context.source.slice(length);
+}
+function isEnd(context, parentTag) {
+  // 2. 遇到结束标签的时候
+  const s = context.source;
+  // if (s.startsWith("</div>")){
+  if (parentTag && s.startsWith(`</${parentTag}>`)){ // 初始化时 parentTag 为空,不需要走此流程
+    return true;
+  }
+  // 1. source 有值的时候,返回 false 不结束
+  return !s;
 }
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -4,7 +4,7 @@ import { baseParse } from "../src/parse";
 describe("Parse", () => {
 
   describe("interpolation", () => {
-    test("simple interpolation", () => {
+    it("simple interpolation", () => {
       const ast = baseParse("{{ message }}");
 
       expect(ast.children[0]).toStrictEqual({
@@ -17,18 +17,19 @@ describe("Parse", () => {
     });
   });
   describe("simple element div", () => {
-    test("simple ele", () => {
+    it("simple ele", () => {
       const ast = baseParse("<div></div>");
 
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.ELEMENT,
         tag: "div",
+        children: []
       });
     });
   });
 
   describe("text", () => {
-    test("simple text", () => {
+    it("simple text", () => {
       const ast = baseParse("some text");
 
       expect(ast.children[0]).toStrictEqual({
@@ -37,5 +38,32 @@ describe("Parse", () => {
       })
     })
   })
+
+  /**
+   * 解析三种联合类型 插值 ele text
+   * <div> hi, {{message}} </div>
+   */
+  test('hello world', () => {
+    const ast = baseParse("<div>hi,{{message}}</div>"); // 需要注意 <div> hi, 前后的空格可能导致测试不通过
+
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: "div",
+      children: [
+        {
+          type: NodeTypes.TEXT,
+          content: "hi,"
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: "message",
+          },
+        }
+      ]
+    })
+  });
+
 
 });
